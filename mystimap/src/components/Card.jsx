c7c8db085991d56965a050172572fd145d646e47
daisy <chpdaisy@gmail.com>
2025-04-25 12:53:24 +0300

    created a function in Card.jsx

--- mystimap/src/components/Card.jsx
+++ mystimap/src/components/Card.jsx
@@ -1,2 +1,18 @@
 import React from 'react';
 
+function Cards(){
+    return(
+        < div className='cards'>
+            <h1>Check Out This EPIC Destinations!</h1>
+        < div className='cards_container'>
+        < div className='cards_wrapper'>
+        < div className='cards'></div>
+        
+
+        </div>
+        </div>
+
+        </div>
+    )
+}
+export default Cards;
