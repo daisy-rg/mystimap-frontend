8979ac611027a04f13a96f4bad2d3dc6838569a9
daisy <chpdaisy@gmail.com>
2025-04-25 13:05:00 +0300

    created  Card.jsx

--- mystimap/src/components/Card.jsx
+++ mystimap/src/components/Card.jsx
@@ -1,15 +1,1 @@
 import React from 'react';
-
-function Cards(){
-    return(
-        < div className='cards'>
-            <h1>Check Out This EPIC Destinations!</h1>
-        < div className='cards_container'>
-        < div className='cards_wrapper'>
-        
-        </div>
-        </div>
-
-        </div>
-    )
-}
